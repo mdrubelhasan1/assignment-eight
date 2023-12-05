390db8d6e051e3ed2978937e922baad102db21bf
mdrubelhasan1 <mdrubelhasan360@gmail.comm>
2023-12-05 13:15:18 +0600

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h3>Knowledge Cafe</h3>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/about us">About us</a>
+                <a href="/login">Login</a>
+                <a href="/signup">Sign Up</a>
+                <a href="/contact us">Contact Us</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
